2748e580a3896d24b31af73b5c6e59115109ad77
Dan Vanderkam <dan@dygraphs.com>
2012-09-17 18:52:33 -0400

    store originals from journals

--- journal.py
+++ journal.py
@@ -13,4 +13,8 @@ def ImportJournal(entries, maker, url_maker=None, dry_run=False):
     url = None
     if url_maker:
       url = url_maker(d)
-    utils.WriteSingleSummary(d, maker=maker, summary=summary, url=url, dry_run=dry_run)
+    utils.WriteSingleSummary(
+        d, maker=maker, summary=summary, url=url, dry_run=dry_run)
+    utils.WriteOriginal(
+        d, maker=maker, dry_run=dry_run,
+        contents=entry.encode('utf8'), filename='journal.txt')
